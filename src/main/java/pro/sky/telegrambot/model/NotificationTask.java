559e8368956401b405d30264eea2fc9547603fa3
Свинухов константин <lopata216@yandex.ru>
2024-03-24 17:31:52 +0300

    телеграм бот

--- src/main/java/pro/sky/telegrambot/model/NotificationTask.java
+++ src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -11,6 +11,7 @@ import java.util.Objects;
 public class NotificationTask {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private Long chat_id;
     private String message;
     private LocalDateTime data;
